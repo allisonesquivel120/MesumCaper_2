19fb98f4aa78267145283c5a1d1003360722279c
Jadyn Harrison <harrisoj29@up.edu>
2026-03-13 14:50:12 -0700

    testing testing one two three

---
+++ app/src/test/java/edu/up/cs301/museumcaper/MuseumCaperStateTest.java
@@ -0,0 +1,18 @@
+package edu.up.cs301.museumcaper;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MuseumCaperStateTest {
+
+    @Test
+    public void makeMarkStolenPaintingsAction() {
+
+    }
+
+    @Test
+    public void MuseumCaperState() {
+
+    }
+}
