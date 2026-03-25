7e41b01f23459fb46366d286c09a82590a11637f
Jadyn Harrison <harrisoj29@up.edu>
2026-03-25 15:10:03 -0700

    human player, new and improved! no update display so we'll have to fix the player id

--- app/src/main/java/edu/up/cs301/museumcaper/MuseumCaperHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/museumcaper/MuseumCaperHumanPlayer.java
@@ -67,78 +67,6 @@ public class MuseumCaperHumanPlayer extends GameHumanPlayer implements OnClickLi
 		return myActivity.findViewById(R.id.main_MuseumCaper);
 	}
 
-	/**
-	 * sets the counter value in the text view
-	 */
-    // maybe omit
-	protected void updateDisplay() {
-        //
-        if(state == null || myActivity == null) return;
-
-        if (playerTurnTextView != null) {
-            int turn = state.getPlayerTurn();
-            // turn 0 = thief , turn 1+ = human
-            if (turn == 0) {
-                playerTurnTextView.setText("Thief's Turn");
-            } else {
-                playerTurnTextView.setText(this.name + "'s Turn");
-            }
-        }
-        LocalGame local = (LocalGame) game;
-        GamePlayer[] players = local.getPlayers();
-
-        //determine who's turn it is
-        int turn = state.getPlayerTurn();
-
-        // safety check
-        if(turn < 0 || turn >= players.length)
-        {
-            playerTurnTextView.setText("Unknown Player's Turn.");
-            return;
-        }
-        String name = players[turn].toString();
-
-        // set the text in the appropriate widget -- adjust player turn
-        playerTurnTextView.setText(name + "'s Turn");
-
-//        if (movementDieButton != null) {
-//            int roll = state.getMovementRoll();
-//
-//            //movementDieButton.setImageResource(getDieDrawable(roll, false));
-//            // only clickable during GUARD_ROLL on the guard's turn
-////            boolean canRollMovement = state.getCurrentPhase() == GamePhase.GUARD_ROLL
-////                    && state.getPlayerTurn() == getPlayerNum();
-////            movementDieButton.setEnabled(canRollMovement);
-////            movementDieButton.setAlpha(canRollMovement ? 1.0f : 0.4f);
-//        }
-
-        if (cameraDieButton != null) {
-            int roll = state.getQuestionRoll();
-            // camera die but not quite yet
-            //cameraDieButton.setImageResource(getDieDrawable(roll, true));
-            boolean canRollQuestion = state.getCurrentPhase() == GamePhase.GUARD_QUESTION
-                    && state.getPlayerTurn() == getPlayerNum();
-            cameraDieButton.setEnabled(canRollQuestion);
-            cameraDieButton.setAlpha(canRollQuestion ? 1.0f : 0.4f);
-        }
-
-        GamePhase phase = state.getCurrentPhase();
-
-//        if (movementDieButton != null) {
-//            movementDieButton.setEnabled(phase == GamePhase.GUARD_ROLL
-//                    && state.getPlayerTurn() == getPlayerNum());
-//        }
-        if (cameraDieButton != null) {
-            cameraDieButton.setEnabled(phase == GamePhase.GUARD_QUESTION
-                    && state.getPlayerTurn() == getPlayerNum());
-        }
-
-    }
-
-		// set the text in the appropriate widget -- adjust player turn
-        //playerTurnTextView.setText(name + "'s Turn");
-
-
     /**
 	 * this method gets called when the user clicks the '+' or '-' button. It
 	 * creates a new CounterMoveAction to return to the parent activity.
@@ -147,18 +75,6 @@ public class MuseumCaperHumanPlayer extends GameHumanPlayer implements OnClickLi
 	 * 		the button that was clicked
 	 */
 	public void onClick(View button) {
-		// if we are not yet connected to a game, ignore
-		// perchance if (game == null) return;
-//        int row = clickedRow;
-//        int col = clickedCol;
-//        int guardIndex = getPlayerNum() - 1;
-//        if(guardIndex >= 0)
-//        {
-//            game.sendAction(new MuseumCaperGuardMoveAction(this, guardIndex, row,col));
-//        }
-        // perchance GameAction cameraAction;
-
-
         if (button.getId() == R.id.regulardie) {
             // send a MOVEMENT dice roll action
             game.sendAction(new MuseumCaperRollDiceAction(this, DiceType.MOVEMENT));
@@ -178,32 +94,26 @@ public class MuseumCaperHumanPlayer extends GameHumanPlayer implements OnClickLi
 	@Override
 	public void receiveInfo(GameInfo info) {
 		// ignore the message if it's not a CounterState message
-		if (!(info instanceof MuseumCaperState)) { return; };
-        // set textviews
+		if (info instanceof MuseumCaperState) {
+            MuseumCaperState newState = new MuseumCaperState((MuseumCaperState) info);
 
-        if (state.getDiceValue() == 1) {
-            movementDieButton.setImageResource(R.drawable.basedie1);
-        } else if (state.getDiceValue() == 2) {
-            movementDieButton.setImageResource(R.drawable.basedie2);
-        } else if (state.getDiceValue() == 3) {
-            movementDieButton.setImageResource(R.drawable.basedie3);
-        } else if (state.getDiceValue() == 4) {
-            movementDieButton.setImageResource(R.drawable.basedie4);
-        } else if (state.getDiceValue() == 5) {
-            movementDieButton.setImageResource(R.drawable.basedie5);
-        } else if (state.getDiceValue() == 6) {
-            movementDieButton.setImageResource(R.drawable.basedie6);
+            if (newState.getDiceValue() == 1) {
+                movementDieButton.setImageResource(R.drawable.basedie1);
+            } else if (newState.getDiceValue() == 2) {
+                movementDieButton.setImageResource(R.drawable.basedie2);
+            } else if (newState.getDiceValue() == 3) {
+                movementDieButton.setImageResource(R.drawable.basedie3);
+            } else if (newState.getDiceValue() == 4) {
+                movementDieButton.setImageResource(R.drawable.basedie4);
+            } else if (newState.getDiceValue() == 5) {
+                movementDieButton.setImageResource(R.drawable.basedie5);
+            } else if (newState.getDiceValue() == 6) {
+                movementDieButton.setImageResource(R.drawable.basedie6);
+            }
+
+            // update our state; then update the display
+            //this.state = (MuseumCaperState)info;
         }
-
-		// update our state; then update the display
-		//this.state = (MuseumCaperState)info;
-        //check error for following line! What does it mean counter state?
-
-        // perchance
-        // state = new MuseumCaperState((MuseumCaperState) info);
-
-		// perchance
-        // updateDisplay();
 	}
 
 	/**
@@ -214,34 +124,21 @@ public class MuseumCaperHumanPlayer extends GameHumanPlayer implements OnClickLi
 	 * 		the activity under which we are running
 	 */
 	public void setAsGui(GameMainActivity activity) {
-
 		// remember the activity
-		this.myActivity = activity;
+		myActivity = activity;
 
 	    // Load the layout resource for our GUI
-		activity.setContentView(R.layout.museumcaper_human_player);
-        // testing commit changes
+        activity.setContentView(R.layout.museumcaper_human_player);
 
-        this.playerTurnTextView = (TextView)myActivity.findViewById(R.id.turnInfo);
-        this.movementDieButton = (ImageButton)myActivity.findViewById(R.id.regulardie);
-        this.cameraDieButton = (ImageButton)myActivity.findViewById(R.id.cameradie);
-
+        //Initialize the widget reference member variables
+        this.playerTurnTextView = (TextView)activity.findViewById(R.id.turnInfo);
+        this.movementDieButton = (ImageButton)activity.findViewById(R.id.regulardie);
+        this.cameraDieButton = (ImageButton)activity.findViewById(R.id.cameradie);
 
         // make both dice clickable
         movementDieButton.setOnClickListener(this);
         cameraDieButton.setOnClickListener(this);
 
-        // also make the die ImageViews clickable in the XML sense
-        //perchance uncomment, let's see it it does anythign!
-//        movementDieButton.setClickable(true);
-//        cameraDieButton.setClickable(true);
-
-		// if we have a game state, "simulate" that we have just received
-		// the state from the game so that the GUI values are updated
-        //perchance
-//		if (state != null) {
-//			receiveInfo(state);
-//		}
 	}//blah blah
 
     @Override
